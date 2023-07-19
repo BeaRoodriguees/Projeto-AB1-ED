ec139cfd4f8092a4398ca7f68e2f4be23c574360
Bea Rodrigues <116564168+BeaRoodriguees@users.noreply.github.com>
2023-07-19 14:34:58 -0300

    update list library

--- list.c
+++ list.c
@@ -2,25 +2,26 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define 1 TRUE
-#define 0 FALSE
+#define TRUE 1
+#define FALSE 0
 
 // definir nomes melhores
-// implementar funções: removerRepetido, removerElemRepetido e qntRepetido
+// implementar funções: removerRepetido
 
-typedef struct{
+typedef struct elem{
     char nome[60];
     int idade;
-    int cpf;
+    int cpf_i;
+    char cpf_c[12];
 } Elem;
 
-typedef struct{
+typedef struct lista{
     Elem *elementos;
     int qnt, tam;
 } ListaE;
 
 int criarLista (ListaE *lista, int tamanho){
-    lista->elementos = calloc(tam, sizeof(Elem));
+    lista->elementos = calloc(tamanho, sizeof(Elem));
 
     if (lista->elementos == NULL) return FALSE;
 
@@ -30,23 +31,16 @@ int criarLista (ListaE *lista, int tamanho){
     return TRUE;
 }
 
-int vaziaLista (ListaE lista){
+int verVazia (ListaE lista){
     return lista.qnt == 0;
 }
 
-int cheiaLista (ListaE lista){
+int verCheia (ListaE lista){
     return lista.qnt == lista.tam;
 }
 
-int quantidade (ListaE lista){
-    return lista.quantidade;
-}
-
-int qntRepetido (ListaE lista){
-}
-
-int inserirInicio (ListaE *lista, Elem novo){
-    if (cheiaLista(*lista)) return FALSE;
+int inInicio (ListaE *lista, Elem novo){
+    if (verCheia(*lista)) return FALSE;
 
     for (int i = lista->qnt; i > 0; i--){
         lista->elementos[i] = lista->elementos[i-1];
@@ -57,20 +51,20 @@ int inserirInicio (ListaE *lista, Elem novo){
     return TRUE;
 }
 
-int inserirFim (ListaE *lista, Elem novo){
-    if (cheiaLista(*lista)) return FALSE;
+int inFim (ListaE *lista, Elem novo){
+    if (verCheia(*lista)) return FALSE;
 
-    lista->elementos[lista->quantidade++] = novo;
+    lista->elementos[lista->qnt++] = novo;
     return TRUE;
 }
 
 void imprimirLista (ListaE lista){
     for (int i = 0; i < lista.qnt; i++){
-        printf("O paciente é %s, de %d anos, com CPF %d.\n", lista.elementos[i].nome, lista.elementos.idade, lista.elementos.cpf);
+        printf("O paciente é %s, de %d anos, com CPF %d.\n", lista.elementos[i].nome, lista.elementos[i].idade, lista.elementos[i].cpf_i);
     }
 }
 
-int pesquisarElem (ListaE lista, char *nome){
+int pesquisarNome (ListaE lista, char *nome){
     for (int i = 0; i < lista.qnt; i++){
         if (strcmp(lista.elementos[i].nome, nome) == 0)
             return i;
@@ -78,8 +72,20 @@ int pesquisarElem (ListaE lista, char *nome){
     return -1;
 }
 
+int pesquisarCPF (ListaE lista, int cpf_i){
+    for (int i = 0; i < lista.qnt; i++){
+        if (lista.elementos[i].cpf_i == cpf_i)
+            return i;
+    }
+    return -1;
+}
+
+int qnt (ListaE lista){
+    return lista.qnt;
+}
+
 int removerInicio (ListaE *lista, Elem *elem){
-    if (vaziaLista(*lista)) return FALSE;
+    if (verVazia(*lista)) return FALSE;
 
     *elem = lista->elementos[0];
     --lista->qnt;
@@ -92,20 +98,16 @@ int removerInicio (ListaE *lista, Elem *elem){
 }
 
 int removerFim (ListaE *lista, Elem *elem){
-    if(vaziaLista(*lista)) return FALSE;
+    if(verVazia(*lista)) return FALSE;
 
     *elem = lista->elementos[--lista->qnt];
     return TRUE;
 }
 
-int remover (ListaE *lista, Elem *elem){
-    int pos = pesquisarElem(*lista, elem->nome);
-    
+int remover (ListaE *lista, int pos){    
     if (pos < 0) return FALSE;
 
-    *elem = lista->elementos[pos];
-
-    for (int i = pos; i < lista.qnt; i++)
+    for (int i = pos; i < lista->qnt; i++)
         lista->elementos[i] = lista->elementos[i+1];
 
     lista->qnt--;
@@ -113,8 +115,77 @@ int remover (ListaE *lista, Elem *elem){
     return TRUE;
 }
 
-void removerRepetido (){
+void qntElem (ListaE lista){
+    int cont = 0;
+    while(!verVazia(lista)){
+        int check = lista.elementos[0].cpf_i;
+       
+        while (pesquisarCPF(lista, check) != -1){
+            int pos = pesquisarCPF(lista, check);
+            cont++;
+            remover(&lista, pos);
+        }
+
+        printf("Contém %d unidade(s) do elemento %d\n", cont, check);
+        cont = 0;
+    }
 }
 
-int removerElemRepetido (ListaE *lista, char elem){
+void removerElemRepetido (ListaE *lista, int item){
+    int count = 0;
+    for (int i = 0; i < lista->qnt; i++){
+        if (lista->elementos[i].cpf_i == item){
+            count++;
+       
+            if (count >= 2){
+                for (int j = i; j < lista->qnt; j++){
+                    lista->elementos[j] = lista->elementos[j+1];
+                }
+
+                lista->qnt--;
+            }
+        }
+    }
+}
+
+void removerElem (ListaE *lista, int item){
+    for (int i = 0; i < lista->qnt; i++){
+        if (lista->elementos[i].cpf_i == item){
+            for (int j = i; j < lista->qnt; j++){
+                lista->elementos[j] = lista->elementos[j+1];
+            }
+            lista->qnt--;
+        }
+    }
+}
+
+void removerRepetidos (ListaE *lista){
+    for (int i = 0; i < lista->qnt; i++){
+        for (int j = i + 1; j < lista->qnt; j++){
+            if (lista->elementos[i].cpf_i == lista->elementos[j].cpf_i){
+                    for (int k = j; k < lista->qnt; k++)
+                        lista->elementos[k] = lista->elementos[k+1];
+                    
+                    lista->qnt--;
+            }
+        }
+    }
+}
+
+void inverterLista (ListaE *lista){
+    ListaE copia;
+    criarLista(&copia, lista->tam);
+    copia.qnt = lista->qnt;
+    copia.tam = lista->tam;
+
+    for (int i = 0; i < lista->qnt; i++){
+        copia.elementos[i] = lista->elementos[lista->qnt-i-1] ;
+    }
+
+    for (int i = 0; i < lista->qnt; i++){
+        lista->elementos[i] = copia.elementos[i] ;
+    }
+
+    free(copia.elementos);
+    
 }
