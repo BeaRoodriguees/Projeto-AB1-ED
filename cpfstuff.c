d5c1360df39b304143e9269c43a03dbb7fea3380
Keeoth <gomesal2009@gmail.com>
2023-07-24 15:49:32 -0300

    funcoes de separar/verificar cpf

--- cpfstuff.c
+++ cpfstuff.c
@@ -3,9 +3,9 @@
 #include <math.h>
 #include <stdlib.h>
 
-void string_Separator(char src_string[], char dst_string[])
+void string_Separator(char src_string[], char dst_string[])//1º parâmetro é a string a ser separada, 2º parâmetro é a string que vai receber a primeira string após a remoção dos tokens.
 {
-    char t[] = ".-";
+    char t[] = ".-"; //tokens a serem removidos da primeira string.
     char *portion = strtok(src_string, t);
 
     while (portion != NULL)
@@ -17,7 +17,8 @@ void string_Separator(char src_string[], char dst_string[])
     return;
 }
 
-int cpf_Verifier(long int cpf) {
+int cpf_Verifier(long int cpf) //Recebe o cpf no formato long int, necessário uso da função atol para converter a string para long int.
+{
     int x = 0;
     
     while (cpf != 0)
@@ -32,7 +33,7 @@ int cpf_Verifier(long int cpf) {
     int a = x % 10;
     int b = (x - a)/10;
 
-    if (a == b) return 1;
+    if (a == b) return 1; //Retorna 1 se o cpf for válido, retorna 0 se for inválido.
 
     return 0;
 }
