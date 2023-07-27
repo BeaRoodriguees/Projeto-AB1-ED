737754ceacd20dbfbb8699c4989883e86ad605ec
Felipe Lira <128178415+LiraCode@users.noreply.github.com>
2023-07-27 01:04:03 -0300

    Fixed data overwrite in .text

--- cadastroPaciente.c
+++ cadastroPaciente.c
@@ -3,18 +3,22 @@
 #include "libs/cpfstuff.c"
 #define MAX_LEN 100
 
-//A saída pro arquivo lisPacients.txt está sendo sobrescrita.
+//falta implentar se o usuário já existe
 
 int main(){
     char nome[MAX_LEN];
     char doenca[MAX_LEN];
-    char cpf_s[16], cpf_save[16], cpf_clean[12];
+    char cpf_s[15], cpf_save[15], cpf_clean[12];
+    char temp[2];
 
     FILE *pacientInfos = fopen("paciente.txt", "r");
     
     fgets(nome, MAX_LEN, pacientInfos);
-    fgets(cpf_s, 16, pacientInfos);
+    nome[strcspn(nome, "\n")] = 0;
+    fgets(cpf_s, 15, pacientInfos);
+    fgets(temp, 2, pacientInfos);
     fgets(doenca, MAX_LEN, pacientInfos);
+    doenca[strcspn(doenca, "\n")] = 0;
 
     strcpy(cpf_save, cpf_s);
     cpf_Separator(cpf_s, cpf_clean);
@@ -27,9 +31,9 @@ int main(){
         return -1;
     }
 
-    FILE *pacientValid = fopen("listPacients.txt", "w");
+    FILE *pacientValid = fopen("listPacients.txt", "ab");
 
-    fprintf(pacientValid, "%s%s%s\n", nome, cpf_save, doenca);
+    fprintf(pacientValid, "%s %s %s\n", nome, cpf_save, doenca);
     fclose(pacientValid);
     fclose(pacientInfos);
     return 0;
